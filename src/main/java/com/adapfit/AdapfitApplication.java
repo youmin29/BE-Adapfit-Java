448ff088aa993b3b470ece3ceff7f4c1115e2b82
youmin29 <babybana0829@naver.com>
2025-05-10 17:50:11 +0900

    🎉 Initial Commit

---
+++ src/main/java/com/adapfit/AdapfitApplication.java
@@ -0,0 +1,12 @@
+package com.adapfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdapfitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdapfitApplication.class, args);
+    }
+}
